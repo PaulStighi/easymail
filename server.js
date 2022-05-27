07d0504c22e56b197fe3cfca7084973b376cb1e7
STIEGELBAUER Paul <paul.stighi@gmail.com>
2022-05-28 00:45:26 +0300

    Mongo setup and corrections

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
 const secretData = require('./sensitiveData.json');
-
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-
 app.use(express.static('public'));
+
+// DB Connection
+
+const db_url = 'mongodb://localhost:27017/mail-pug-inator';
+
+mongoose.connect(db_url,
+    { useNewUrlParser: true },
+    function () {
+        console.log('connected to database');
+    }
+);
+
+// App init
+
 app.use(express.json());
 app.set('view engine', 'pug');
-app.get('/', (req, res) => res.sendFile(__dirname + 'index.html'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + 'index.html');
+});
 
 app.use('/sendmail', require('./routes/Sender'));
-// app.use('/template', require('./routes/Template'));
 
 app.listen(secretData.port, () => console.log(`Server running on port ${secretData.port}`));
