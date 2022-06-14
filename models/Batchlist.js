692356404fa9881ef9b111e70c5c3647b0f86890
STIEGELBAUER Paul <paul.stighi@gmail.com>
2022-06-14 17:13:04 +0300

    Schemas clean-up

--- models/Batchlist.js
+++ models/Batchlist.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const batchlistSchema = new mongoose.Schema({
     to: [
         { type: mongoose.Schema.Types.String }
-    ],
+    ]
 }, { collection: 'batchlists' });
 
 module.exports = mongoose.model('Batchlist', batchlistSchema);
--- models/Template.js
+++ models/Template.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 
 const templateSchema = new mongoose.Schema({
     content: { type: mongoose.Schema.Types.String },
-
 }, { collection: 'templates' });
 
 module.exports = mongoose.model('Template', templateSchema);
